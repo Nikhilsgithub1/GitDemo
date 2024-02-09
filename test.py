7874d5ace0b4e8ed84d2f2fe7262ded3d489f356
Nikhilsgithub1/ <nikhil.official101@gmail.com>
2024-02-09 20:11:58 +0530

    fhf

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-Print("This is a test for second file ")
-=======
 Print("This is a test for second file ")
 
 
@@ -9,4 +6,3 @@ Print("change done  from git hub")
 Print("change done  from git hub")
 
 Print("change done  from git hub222222222")
->>>>>>> 30e695b795f5ef07a085d6f2de0005c5d4b30b99
